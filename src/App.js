9fb3a3adc15397f9d38c991b7438f52ad890437d
Shailesh K <shaileshkachhara89@gmail.com>
2023-08-08 16:20:51 +0530

    azure setup.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { authProvider } from './authProvider';
+import { AzureAD, AuthenticationState } from 'react-aad-msal';
 function App() {
+ 
   return (
+    
     <div className="App">
+ <AzureAD provider={authProvider}>
+      <span>Only authenticated users can see me.</span>
+    </AzureAD>
+ 
+ <AzureAD provider={authProvider} forceLogin={true}>
+  {
+    ({login, logout, authenticationState, error, accountInfo}) => {
+      switch (authenticationState) {
+        case AuthenticationState.Authenticated:
+          return (
+            <p>
+              <span>Welcome, {accountInfo.account.userName}!</span>
+              <span>{accountInfo.jwtIdToken}</span>
+              <button onClick={logout}>Logout</button>
+            </p>
+          );
+        case AuthenticationState.Unauthenticated:
+          return (
+            <div>
+              {error && <p><span>An error occured during authentication, please try again!</span></p>}
+              <p>
+                <span>Hey stranger, you look new!</span>
+                <button onClick={login}>Login</button>
+              </p>
+            </div>
+          );
+        case AuthenticationState.InProgress:
+          return (<p>Authenticating...</p>);
+      }
+    }
+  }
+</AzureAD>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,5 +56,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
